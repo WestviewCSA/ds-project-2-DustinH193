e8a31bd1de804ca079a103a2c190d3254fd1c222
1931856 <1931856@01-18117.pusd.dom>
2025-03-14 14:00:47 -0700

    Finished required maps

--- P2_first_last/src/Map.java
+++ P2_first_last/src/Map.java
@@ -1,4 +1,6 @@
 
 public class Map {
 
+	private Tile [][] map; //is 2D enough?
+	
 }
--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -7,7 +7,7 @@ public class p2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("p2");
-		
+
 		readMap("TestO1");
 	}
 
@@ -17,10 +17,29 @@ public class p2 {
 			File file = new File(filename);
 			Scanner scanner= new Scanner(file);
 			
-			int numRows = scanner.nextInt();
-			int numCols = scanner.nextInt();
+			int numRows  = scanner.nextInt();
+			int numCols  = scanner.nextInt();
 			int numRooms = scanner.nextInt();
 
+			int rowIndex = 0;
+			
+			//process the map!
+			while(scanner.hasNextLine()) {
+				//grab a line (one row of the map0
+				String row = scanner.nextLine();
+				
+				if(row.length()>0) {
+					for(int i = 0; i < numCols && i < row.length(); i++) {
+						char el = row.charAt(i);
+						Tile obj = new Tile(rowIndex, i, el);
+					}
+				}
+				
+		}
+			
+			
+			
+			
 		} catch (FileNotFoundException e) {
 			System.out.println(e);
 			//handle exception
