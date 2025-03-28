0cdc6436aa83c4eb5213fb8cda90fb415e9fcd3e
1931856 <1931856@01-18117.pusd.dom>
2025-03-28 10:35:08 -0700

    kind of trying to understand the concept still.

--- P2_first_last/src/Map.java
+++ P2_first_last/src/Map.java
@@ -2,5 +2,5 @@
 public class Map {
 
 	private Tile [][] map; //is 2D enough?
-	
+	private boolean visited;
 }
--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class p2 {
+public class p2 extends Map {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -12,38 +12,71 @@ public class p2 {
 	}
 
 	public static void readMap(String filename) {
-		
+
 		try {
 			File file = new File(filename);
 			Scanner scanner= new Scanner(file);
-			
+
 			int numRows  = scanner.nextInt();
 			int numCols  = scanner.nextInt();
 			int numRooms = scanner.nextInt();
 
 			int rowIndex = 0;
-			
+
 			//process the map!
 			while(scanner.hasNextLine()) {
-				//grab a line (one row of the map0
+				//grab a line (one row of the map)
 				String row = scanner.nextLine();
-				
+
 				if(row.length()>0) {
 					for(int i = 0; i < numCols && i < row.length(); i++) {
 						char el = row.charAt(i);
 						Tile obj = new Tile(rowIndex, i, el);
 					}
 				}
-				
+
 		}
+				//have you been to the same tile before?
+		
 			
-			
-			
-			
+
 		} catch (FileNotFoundException e) {
 			System.out.println(e);
 			//handle exception
 		}
-		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	}
 }
